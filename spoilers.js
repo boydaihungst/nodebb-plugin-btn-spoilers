d5254d9098ebc44ad58c6b9b65a17d9757fc5f1f
Danijel Marjanovic <dmarjanovic@smithmicro.com>
2017-04-10 13:48:24 +0200

    (minor) variable name tweak

--- spoilers.js
+++ spoilers.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var BTPSpoilers = {
+var BTNSpoilers = {
     parse: function(postData, callback) {
         postData.postData.content = postData.postData.content
             .replace(/\#\#spoiler/g, '<a href="#" class="show-spoiler btn btn-md btn-default waves-effect"><i class="fa fa-eye-slash fa-fw"></i> Spoiler!</a><div class="spoiler hidden">')
@@ -10,4 +10,4 @@ var BTPSpoilers = {
     }
 };
 
-module.exports = BTPSpoilers;
+module.exports = BTNSpoilers;
