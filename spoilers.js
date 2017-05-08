db4b8fa36a53cdd96ae7e8e74524c4cae05441d8
Danijel Marjanovic <dmarjanovic@smithmicro.com>
2017-05-08 16:55:58 +0200

    modifying behavior

--- spoilers.js
+++ spoilers.js
@@ -2,10 +2,10 @@
 
 var NSFWSpoilers = {
     parse: function(postData, callback) {
+        console.log(postData.postData.content)
         postData.postData.content = postData.postData.content
-            //.replace(/\#\#spoiler/g, '<a href="#" class="show-spoiler btn btn-md btn-default waves-effect" tooltip="open at your own discretion"><i class="fa fa-eye-slash fa-fw"></i><span>NSFW!</span></a><div class="spoiler hidden">')
-            .replace(/\#\#spoiler/g, '<div><div href="#" class="show-spoiler btn btn-md btn-default waves-effect" title="Open at your own discretion"><i class="fa fa-eye-slash fa-fw"></i><span> NSFW!</span></div><div class="spoiler hidden">')
-            //.replace(/\#\#endspoiler/g, '</div>')
+            .replace(/\#\#spoiler=([^<]*)/g, '***' + '$1' + '***' + '##spoiler')
+            .replace(/\#\#spoiler/g, '<div><div href="#" class="show-spoiler btn btn-md btn-default waves-effect" title="Open at your own discretion"><i class="fa fa-eye-slash fa-fw"></i><span class="btn-text" data-show_text="click to show" data-hide_text="click to hide">click to show</span></div><div class="spoiler hidden">')
             .replace(/\#\#endspoiler/g, '</div></div>')
 
         callback(null, postData);
--- static/spoilers.js
+++ static/spoilers.js
@@ -1,12 +1,14 @@
 $('document').ready(function() {
     require(['composer', 'composer/controls'], function(composer, controls) {
         composer.addButton('fa fa-eye-slash', function(textarea, selectionStart, selectionEnd) {
+
+            var spoilersTitle = "=NSFW content";
             if(selectionStart === selectionEnd){
-                controls.insertIntoTextarea(textarea, "##spoiler\n\n##endspoiler");
-                controls.updateTextareaSelection(textarea, selectionStart + 11, selectionStart + 11);
+                controls.insertIntoTextarea(textarea, "##spoiler=" + spoilersTitle + "\n\n##endspoiler");
+                controls.updateTextareaSelection(textarea, selectionStart + 11 + spoilersTitle.length, selectionStart + 11 + spoilersTitle.length);
             } else {
-                controls.wrapSelectionInTextareaWith(textarea, '##spoiler\n\n','\n##endspoiler');
-                controls.updateTextareaSelection(textarea, selectionStart + 11, selectionEnd + 11);
+                controls.wrapSelectionInTextareaWith(textarea, '##spoiler' + spoilersTitle + '\n\n','\n##endspoiler');
+                controls.updateTextareaSelection(textarea, selectionStart + 11 + spoilersTitle.length, selectionEnd + 11 + spoilersTitle.length);
             }
         });
     });
@@ -14,6 +16,11 @@ $('document').ready(function() {
     $('body').on('click', 'div.show-spoiler', function(){
         $(this).find('.fa').toggleClass('fa-eye');
         $(this).find('.fa').toggleClass('fa-eye-slash');
+        if($(this).find('.fa').hasClass('fa-eye')){
+            $(this).find('.btn-text').first().text($(this).find('.btn-text').first().data('hide_text'));
+        } else {
+            $(this).find('.btn-text').first().text($(this).find('.btn-text').first().data('show_text'));
+        }
         $(this).parent().find('> .spoiler').toggleClass('hidden');
     });
 
