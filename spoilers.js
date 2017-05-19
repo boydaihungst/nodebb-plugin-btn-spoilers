0370e3733191e4b429ae6d10b2d4b4b360826844
Danijel Marjanovic <dmarjanovic@smithmicro.com>
2017-05-19 15:00:15 +0200

    remove backend console.log()

--- spoilers.js
+++ spoilers.js
@@ -2,7 +2,7 @@
 
 var NSFWSpoilers = {
     parse: function(postData, callback) {
-        console.log(postData.postData.content)
+        // console.log(postData.postData.content)
         postData.postData.content = postData.postData.content
             .replace(/\#\#spoiler=([^<]*)/g, '***' + '$1' + '***' + '##spoiler')
             .replace(/\#\#spoiler/g, '<div><div href="#" class="show-spoiler btn btn-md btn-default waves-effect" title="Open at your own discretion"><i class="fa fa-eye-slash fa-fw"></i><span class="btn-text" data-show_text="click to show" data-hide_text="click to hide">click to show</span></div><div class="spoiler hidden">')
